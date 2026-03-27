ede868dbf2a10e726da0aee025e95ee359b25f44
Yasmi Kalpage <yazmikalpage@gmail.com>
2026-03-28 01:28:32 +0530

    Delete e-learning-backend/routes directory
    move it to the correct path

--- e-learning-backend/routes/progressRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getCourseProgressDetails } = require("../controllers/progressController.js");
-const { authenticate } = require("../middleware/authMiddleware.js");
-
-const router = express.Router();
-
-router.get("/:courseId", authenticate, getCourseProgressDetails);
-
-module.exports = router;
