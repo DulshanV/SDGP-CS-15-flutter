d04ccedc0c3c411eaa581ee14b11aeedae36ef40
Yasmi Kalpage <yazmikalpage@gmail.com>
2026-03-28 01:32:56 +0530

    Delete routes directory
    move it to the right directory

--- routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getUsers } = require("../controllers/userController.js");
-const { authenticate, authorize } = require("../middleware/authMiddleware.js");
-
-const router = express.Router();
-
-router.get("/", authenticate, authorize("admin"), getUsers);
-
-module.exports = router;
